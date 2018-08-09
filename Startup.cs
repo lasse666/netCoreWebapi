4434273f0b413b559d04d5d49fa6b75b61b7ba7b
彭帅 <792657269@qq.com>
2018-08-09 18:50:16 +0800

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JianLian.App.Biz;
+using JianLian.App.IBiz;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace JianLian.App.NetCore.Api
+{
+    public class Startup
+    {
+        //public Startup(IConfiguration configuration)
+        //{
+        //    Configuration = configuration;
+        //}
+
+        private readonly IHostingEnvironment _hostingEnv;
+
+        public Startup(IHostingEnvironment hostingEnv)
+        {
+            _hostingEnv = hostingEnv;
+        }
+        
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            //注册方法
+            services.AddSingleton<IStudentService, StudentService>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            //开发环境
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();//使用开发环境的错误页面
+            }
+
+            app.UseMvc();
+        }
+    }
+}
